1e534101095c9ec41ea909510e006d07140a7d98
ggomajae <ggomajae@gmail.com>
2020-11-26 22:41:42 +0900

    api 모듈 설정

---
+++ src/api/github.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+
+export type GithubProfile = {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  name: string;
+  company: string;
+  blog: string;
+  location: null;
+  email: null;
+  hireable: null;
+  bio: string;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export async function getUserProfile(username: string) {
+
+  const response = await axios.get<GithubProfile>(
+    `https://api.github.com/users/elin-oh`
+  );
+  return response.data; 
+}
